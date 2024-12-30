0f1f9e701b286e4014b46ba2651f22385e0fe573
Jan <159197308+Johan5140@users.noreply.github.com>
2024-12-30 15:14:14 +0100

    Delete navestidlo directory

--- navestidlo/data/js/menu.js
+++
@@ -1,8 +0,0 @@
-function openmenu() {
-  var x = document.getElementById("menu");
-  if (x.className === "menulinks") {
-    x.className += " responsive";
-  } else {
-    x.className = "menulinks";
-  }
-}
