60d326236fa3f15bd9e8551a2e50366a160cf517
Jan <159197308+Johan5140@users.noreply.github.com>
2025-01-17 23:10:46 +0100

    Přidání funkcí a opravy

--- navestidlo/data/js/signal.js
+++ navestidlo/data/js/signal.js
@@ -1,5 +1,6 @@
 var t = null;
 var navests;
+var ynavest;
 function blink (x,y)
 {   
     if (y === undefined){
@@ -18,6 +19,7 @@ function blink (x,y)
         }
 }*/
 function submit () {
+        reload();
      navests = document.getElementById("navesti").value;
      navest(navests);
 }
@@ -29,36 +31,95 @@ function reload () {
         document.getElementById("x5").style.visibility="hidden";
         document.getElementById("x6").style.visibility="hidden";
         document.getElementById("nazev").textContent="---";
-        document.getElementById("vzhlednavesti").textContent="---";
         document.getElementById("popisnavesti").textContent="---";
         document.getElementById("1line").style.backgroundColor = "#000000";
         document.getElementById("2line").style.backgroundColor = "#000000";
         clearInterval(t);
 }
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        navest(this,navests);
+      }
+    };
+    xmlhttp.open("GET", "/navestidlo/data/xml/data.xml", true);
+    xmlhttp.send();
+  }
 function navest (kodnavesti) {
+    /*parser = new DOMParser();
+    doc = parser.parseFromString(text,"text/xml");
+    docx = parser.parseFromString()*/
 
-    var request = new XMLHttpRequest();
-    request.open("GET", "/navestidlo/data/xml/data.xml", true);
+    //const xml = "/navestidlo/data/xml/data.xml"
+
+    fetch("data/xml/data.xml")
+    .then((response) => response.text())
+    .then((xmlString) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.
+            parseFromString(xmlString, "text/xml");
+
+        const xnavesti = xmlDoc.
+        querySelectorAll(kodnavesti);
+        xnavesti.forEach((xnavest) => {
+        const xn = xnavest.
+            querySelector("xa").textContent;
+        const nazev = xnavest.
+            querySelector("nazev").textContent;
+            const xpopis = xnavest.
+            querySelector("popis").textContent;
+            const xblik = xnavest.
+            querySelector("blik").textContent;
+            const xblik2 = xnavest.
+            querySelector("blikx").textContent;
+            if (xn != ""){
+              let k = xn;
+            const myArray = xn.split(";");
+            for (var m = 0, len = myArray.length; m < len; m++) {
+               document.getElementById(myArray[m]).style.visibility="visible"; 
+            }
+            }
+            if (xblik == "pomalu"){
+              var blink_speed = 900; // every 1000 == 1 second, adjust to suit
+         t = setInterval(function () {
+          var ele = document.getElementById(xblik2);
+          ele.style.visibility = (ele.style.visibility == 'visible' ? '' : 'visible');
+          }, blink_speed);
+            } 
+            if (xblik == "rychle"){
+              var blink_speed = 450; // every 1000 == 1 second, adjust to suit
+         t = setInterval(function () {
+          var ele = document.getElementById(xblik2);
+          ele.style.visibility = (ele.style.visibility == 'visible' ? '' : 'visible');
+          }, blink_speed);
+            }
+            
+            
+            document.getElementById("nazev").innerHTML = nazev;
+            document.getElementById("popisnavesti").innerHTML = xpopis;
+        console.log(
+            `Tutorial ID: Title: ${nazev}, Title: ${xblik}, Title: ${xblik2}`);
+    });
+    });
+
+/*    var request = new XMLHttpRequest();
+    request.open("GET", "/navestidlo/data/xml/data.xml", false);
     request.send();
     var xml = request.responseXML;
-    var ynavest = xml.getElementsByTagName(kodnavesti);
-    if(ynavest != null){
-        var xa = ynavest.getElementsByTagName("xa")[0].childNodes[0].nodeValue;
-        /*var xr = ynavest.getElementsByTagName("xr");
-        var blik = ynavest.getElementsByTagName("blik");
-        var blikx = ynavest.getElementsByTagName("blikx");
-        var blikx2 = ynavest.getElementsByTagName("blikx2");
-        var nazev = ynavest.getElementsByTagName("nazev");
-        var indikatordole = ynavest.getElementsByTagName("indikatordole");
-        var linka1 = ynavest.getElementsByTagName("linka1");
-        var linka2 = ynavest.getElementsByTagName("linka2");
-        var svetla = ynavest.getElementsByTagName("svetla");
-        var popis = ynavest.getElementsByTagName("popis");*/
+    var xn = xml.getElementsByTagName(kodnavesti);
+    for(var i = 0; i < xn.length; i++) {
+        var xnn = xn[i];
+        var xna = xnn.getElementsByTagName("xa");
+        var xnaz = xnn.getElementsByTagName("nazev");
+        var xpop = xnn.getElementsByTagName("popis");
+        //var xpop = xnn.getElementsByTagName("popis");
+        */
+       /* for(var k = 0; k < xnaz.length; k++) {
+            document.getElementById("nazev").innerHTML = "" + xnaz[k].childNodes[0].nodeValue
+        }*/
+       /* for(var n = 0; n < xpop.length; n++) {
+            //document.getElementById("popisnavesti").innerHTML = "" + xpop[n].childNodes[0].nodeValue
+        }*/
     }
-    getElementsByTagName(xa).style.visibility="visible";
-
-    document.getElementById("nazev").innerHTML= kodnavesti;
-//document.getElementById("demo").innerHTML = txt;
-
-}
 
